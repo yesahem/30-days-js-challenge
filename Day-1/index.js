a10927057692d880491915a745e00d60ba61b087
iitanahem <ahemraj82@g>
2024-07-13 16:23:54 +0530

    Day-1 Challenge

---
+++ Day-1/index.js
@@ -0,0 +1,36 @@
+var firstVariable = 34
+console.log(firstVariable)
+
+
+
+let secondVariable = 56
+console.log(secondVariable)
+
+
+const thirdVariable = false
+console.log(thirdVariable)
+
+
+const fourthVariable = 34
+const fifthVariable = true
+const sixthVariable = "Hi There"
+const seventhVariable = {
+  "key": "This is an object"
+}
+const eighthVariable = ["This is an array", 69]
+
+console.log(typeof fourthVariable)
+console.log(typeof fifthVariable)
+console.log(typeof sixthVariable)
+console.log(typeof seventhVariable)
+console.log(typeof eighthVariable)
+
+
+
+let ninthVariable = 34
+console.log(ninthVariable)
+ninthVariable = "Changed the value of the variable"
+console.log(ninthVariable)
+
+
+
