c391fb4749b90dddf0453fc00f11f1ee7d2c30f2
iitanahem <ahemraj82@g>
2024-07-14 11:40:47 +0530

    Day-2

---
+++ Day-2/index.js
@@ -0,0 +1,44 @@
+
+let variable1 = 34
+let variable2 = 45
+const variable3 = true
+const addition = variable1 + variable2
+console.log(addition)
+
+const subtraction = variable2 - variable1
+console.log(subtraction)
+
+
+const multiplication = variable1 * variable2
+console.log(multiplication)
+
+const division = variable2 / variable1
+console.log(division)
+
+const reminder = variable2 % variable1
+console.log(reminder)
+
+variable1 += 24
+console.log(variable1)
+
+variable2 -= 5
+console.log(variable2)
+
+//comparision operators
+variable1 > variable2 ? console.log("variable1 is greater than variable 2") : console.log("variable2 is greater than variable 1")
+variable1 >= variable2 ? console.log("variable1 is greater than or equals variable 2") : console.log("variable2 is greater than variable 1")
+variable1 == variable2 ? console.log("variable1 is equals to variable 2") : console.log("variable1 is not equals to variable2 ")
+
+
+variable1 && variable2 == true ? console.log("Both variable consists true value") : console.log("One of the varible is false")
+variable1 || variable3 == true ? console.log("One of the variable is true") : console.log("Both of the variable is false")
+!variable3 ? console.log("value is true") : console.log("value is false")
+
+
+
+
+variable1 > 0 ? console.log("Value is Positive") : console.log("Value is negative")
+
+
+
+
